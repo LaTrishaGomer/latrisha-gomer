424ee69f93fa053e6fa847af88365c2ddcba5f88
latrisha <l_gomer@yahoo.com>
2026-03-28 23:51:15 -0700

    add blog subheader

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -6,8 +6,8 @@ import { Github, ExternalLink } from 'lucide-react'
 const Work = () => {
   return (
      <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-      {/*<h4 className='text-center mb-2 text-lg font-ovo'>My portfolio</h4>
-      <h2 className='text-center text-5xl font-ovo'>My latest work</h2>
+      <h4 className='mb-2 text-lg font-ovo max-w-6xl mx-auto'>The blog</h4>
+      {/*<h2 className='text-center text-5xl font-ovo'>My latest work</h2>
 
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
         Here are some of my latest projects. I designed and developed these apps and websites using tools like Figma, Node.js, Express, MongoDB, EJS, HTML, CSS, and JavaScript.
