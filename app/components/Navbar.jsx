0d6cb6cbad0fd385e460decd8b47c702d4f88136
latrisha <l_gomer@yahoo.com>
2026-03-29 11:48:09 -0700

    reroute blog navbar to blog page. reroute home instead of #top

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -38,8 +38,8 @@ const Navbar = () => {
 
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8
         rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50" } `}>
-            <li><a className='font-Ovo' href="#top">Home</a></li>
-            <li><a className='font-Ovo' href="/#blog">Blog</a></li>
+            <li><a className='font-Ovo' href="/home">Home</a></li>
+            <li><a className='font-Ovo' href="/blog">Blog</a></li>
              <li><a className='font-Ovo' href="/about">About</a></li>
            {/*<li><a className='font-Ovo' href="#services">Services</a></li>*/}
             {/*<li><a className='font-Ovo' href="/contact">Let's Chat</a></li>*/}
